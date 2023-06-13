a3b29f284d0fa7b16af2ebb46f7e5afb1f1968c9
Gassayping <Dev_Gassayping@proton.me>
2023-06-13 14:40:03 -0700

    Setting up git repo for the task manager plugin

--- main.ts
+++ main.ts
@@ -1,137 +1,121 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Plugin, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface Task {
+	name: string,
+	length: number
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AnotherTaskManager extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
+		console.log('Another Task Manager: Loaded');
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			'id': 'atm-new-task',
+			'name': 'New Task',
+			'editorCallback': (editor) => {
+				new TaskModal(this.app, (tasks) => {
+					const taskText: string[] = [];
+					let startTime = new Date(Date.now()).setMinutes((Math.ceil(new Date(Date.now()).getMinutes() / 5) * 5), 0, 0);
+					tasks.forEach((task, idx) => {
+						const taskTime = new Date(startTime += ((tasks[idx - 1]?.length ?? 0) * 1000 * 60));
+						const timeText = `${taskTime.getHours()}:${taskTime.getMinutes() > 9 ? taskTime.getMinutes() : '0' + taskTime.getMinutes()}`;
+						taskText.push(`- [ ] ${timeText} - ${task.name}`)
+					});
+					editor.replaceRange(taskText.join('\n'), editor.getCursor());
+				}).open();
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		})
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		console.log('Another Task Manager: Unloaded');
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class TaskModal extends Modal {
+	tasks: Task[];
+	taskCount: number;
+	onSubmit: (tasks: Task[]) => void;
+
+	constructor(app: App, onSubmit: (tasks: Task[]) => void) {
 		super(app);
+		this.taskCount = 0;
+		this.onSubmit = onSubmit;
+		this.tasks = [];
+	}
+
+	private createTask() {
+		const { contentEl } = this;
+		const idx = this.taskCount++;
+		//const thisTask = contentEl.childNodes[idx]
+		this.tasks.push({
+			name: String(this.taskCount),
+			length: 5
+		})
+		new Setting(contentEl)
+			.setName(`Task ${this.taskCount}`)
+			.addText((text) => {
+				text.setPlaceholder('Name')
+					.onChange((name) => {
+						this.tasks[idx].name = name
+					})
+			})
+
+			.addDropdown((dropdown) => {
+				dropdown
+					.addOption('1', '1 Minute')
+					.addOption('5', '5 Minutes')
+					.addOption('10', '10 Minutes')
+					.addOption('15', '15 Minutes')
+					.addOption('20', '20 Minutes')
+					.addOption('25', '25 Minutes')
+					.addOption('30', '30 Minutes')
+					.setValue('5')
+					.onChange((choice) => {
+						this.tasks[idx].length = parseInt(choice);
+					})
+			})
+		/* .addButton((button) => {
+			button
+				.setIcon('trash-2')
+				.setTooltip('Delete Task', { 'placement': 'right' })
+				.setWarning()
+				.onClick((_ev) => {
+					contentEl.removeChild(thisTask);
+					this.taskCount--;
+				})
+		}) */
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText('Create new task schedule');
+		new Setting(contentEl)
+			.addButton((newTask) => {
+				newTask
+					.setButtonText('New Task')
+					.setTooltip('Create a new task in the list', { 'placement': 'top' })
+					.onClick((_ev) => {
+						this.createTask();
+					})
+			})
+			.addButton((create) => {
+				create
+					.setButtonText('Create')
+					.setTooltip('Finalize and Create Routine', { 'placement': 'top' })
+					.setCta()
+					.onClick((_ev) => {
+						this.close();
+						this.onSubmit(this.tasks);
+					});
+			});
+		this.createTask();
+
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
